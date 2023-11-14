7d486215ff02ddbce378de865d2039d5ea0c5a3f
Abdias Saint Louis <abiwriter.sp@gmail.com>
2023-11-14 11:21:23 -0300

    Initial commit

---
+++ src/main/java/io/github/AbdiasSaintLouis/PicDrop/PicDropApplication.java
@@ -0,0 +1,13 @@
+package io.github.AbdiasSaintLouis.PicDrop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicDropApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicDropApplication.class, args);
+	}
+
+}
